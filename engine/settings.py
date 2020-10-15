20a4bdb717a4498dac170dc7c35cc03f7be07e3d
Sergio Andrés Alvarez Medina <ser.alvarez1998@gmail.com>
2020-10-14 23:35:37 -0300

    Added basic settings to the engine

---
+++ engine/settings.py
@@ -0,0 +1,10 @@
+"""Python file with all the important constants and global variables of the engine."""
+
+# Screen settings
+WIDTH = 800
+HEIGHT = 600
+
+clear_color = [0.15, 0.15, 0.15, 1]  # clear color of the screen
+
+# type settings
+FLOAT_BYTES = 4  # float seran representados en 4 bytes.
