1ac02aad6522b0105a7223a2ae77ebd63294c69d
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2020-10-20 20:30:29 -0300

    Added brief description of the main file

--- src/main.py
+++ src/main.py
@@ -1,3 +1,9 @@
+"""
+Main file of the relief application.
+
+Starts the main program, calling the engine and the logic.
+"""
+
 # TODO: Write docs for this file
 # TODO: Make a program and load a file
 # TODO: Render more tha just a square in the screen
