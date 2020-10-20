f477c7fc86e3156b3db2b2479b7137c240fc2375
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2020-10-20 19:55:45 -0300

    Created function to get logger

--- src/main.py
+++ src/main.py
@@ -2,4 +2,24 @@
 # TODO: Make a program and load a file
 # TODO: Render more tha just a square in the screen
 # TODO: Generate a triangulation from the points
+# TODO: Search for an interfave for the app (IMGUI related)
 
+import logging
+
+def get_logger(log_level: int) -> None:
+    """
+    Get the logger to use in the main program.
+
+    Args:
+        log_level: Level too show in the logs (logging.DEBUG, logging.INFO, ...)
+
+    Returns: None
+
+    """
+
+
+
+if __name__ == '__main__':
+
+    # TODO: Create the main wiindow for the program
+    print("Relief Creator v0.1")
