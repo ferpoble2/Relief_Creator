85ccb2e77c617153fb61f019ffa98c41c80bb26c
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2020-11-11 16:11:28 -0300

    Moved interpolate function

--- src/engine/model/map2dmodel.py
+++ src/engine/model/map2dmodel.py
@@ -18,24 +18,7 @@ from src.engine.data import decimation
 from src.engine.settings import WIDTH
 from src.engine.settings import HEIGHT
 from src.engine.model.tranformations.transformations import ortho
-
-
-def interpolate(value: float, value_min: float, value_max: float, target_min: float = -1,
-                target_max: float = 1):
-    """
-
-    Args:
-        value: Value to interpolate.
-        value_min: Minimum value of the values.
-        value_max: Maximum value of the values.
-        target_min: Minimum value of the interpolation interval.
-        target_max: Maximum value of the interpolation interval.
-
-    Returns: Interpolated value.
-
-    """
-    return (float(value) - value_min) * (float(target_max) - target_min) / (float(value_max) - value_min) + target_min
-
+from src.engine.utils import interpolate
 
 class Map2DModel(Model):
     """
