5d80bae027fed32a3866d2bc3f7564e55fc701a9
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2020-12-13 14:20:49 -0300

    updateado controlador

--- src/engine/controller/controller.py
+++ src/engine/controller/controller.py
@@ -5,8 +5,8 @@ import sys
 from typing import Callable
 
 import glfw
-from src.engine.settings import SCENE_BEGIN_X, SCENE_BEGIN_Y
-from src.engine.render import Render
+# from src.engine.engine import Engine
+from src.engine.settings import Settings
 from src.utils import get_logger
 
 log = get_logger(module="CONTROLLER")
@@ -22,17 +22,19 @@ class Controller:
         Constructor of the class.
         """
         self.__render = None
+        self.__scene = None
 
-    def init(self, render: Render) -> None:
+    def init(self, engine: 'Engine') -> None:
         """
         Initialize the Controller component.
 
         Args:
-            render: Render to be used by the application.
+            engine: Engine used in the application
 
         Returns: None
         """
-        self.__render = render
+        self.__render = engine.render
+        self.__scene = engine.scene
 
     @staticmethod
     def get_on_key_callback() -> Callable:
@@ -63,6 +65,9 @@ class Controller:
 
         def on_resize(window, width, height):
             log.debug(f"Windows resized to {width}x{height}")
-            self.__render.change_viewport(SCENE_BEGIN_X, SCENE_BEGIN_Y, width, height)
+            Settings.HEIGHT = height
+            Settings.WIDTH = width
+            Settings.update_scene_values()
+            self.__scene.update_viewport()
 
         return on_resize
