167205f9b8de42be401690c3481f36ceaced6710
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2020-12-31 21:18:26 -0300

    Sorted methods in scene

--- src/engine/scene/scene.py
+++ src/engine/scene/scene.py
@@ -28,25 +28,15 @@ class Scene:
         # -------------------
         self.__should_execute_then_reload = 0
 
-    def initialize(self, engine: 'Engine') -> None:
+    def add_model(self, model: Model) -> None:
         """
-        Initialize the component in the engine.
+        Add a model to the list of models.
         Args:
-            engine: Engine to use
+            model: Model to add to the list
 
         Returns: None
         """
-        self.__engine = engine
-
-    def update_viewport_variables(self):
-        """
-        Update the viewport variables.
-
-        Returns: None
-        """
-        viewport_data = self.__engine.get_scene_setting_data()
-        self.__width_viewport = viewport_data['SCENE_WIDTH_X']
-        self.__height_viewport = viewport_data['SCENE_HEIGHT_Y']
+        self.__model_list.append(model)
 
     def draw(self) -> None:
         """
@@ -58,115 +48,32 @@ class Scene:
         for model in self.__model_list:
             model.draw()
 
-    def add_model(self, model: Model) -> None:
+    def get_float_bytes(self) -> int:
         """
-        Add a model to the list of models.
+        Get the float bytes used in a float to render.
+        Ask the engine for this information (that is stored in the settings).
+
+        Returns: Number of bytes used for store float numbers.
+        """
+        return self.__engine.get_float_bytes()
+
+    def get_scene_setting_data(self) -> dict:
+        """
+        Get the scene settings.
+
+        Returns: None
+        """
+        return self.__engine.get_scene_setting_data()
+
+    def initialize(self, engine: 'Engine') -> None:
+        """
+        Initialize the component in the engine.
         Args:
-            model: Model to add to the list
+            engine: Engine to use
 
         Returns: None
         """
-        self.__model_list.append(model)
-
-    def remove_all_models(self) -> None:
-        """
-        Remove all models from the list of models.
-        Returns: None
-        """
-        self.__model_list = []
-
-    def reload_models_async(self, then):
-        """
-        Ask the 2D models to reload with the new resolution of the screen.
-
-        Returns: None
-
-        Args:
-            then: Function to be called after all the async routine.
-        """
-
-        # set up the then routine that should executes only once.
-        # -------------------------------------------------------
-        self.__should_execute_then_reload = len(self.__model_list)
-
-        def then_routine():
-            self.__should_execute_then_reload -= 1
-
-            if self.__should_execute_then_reload == 0:
-                self.__should_execute_then_reload = len(self.__model_list)
-                then()
-
-        for model in self.__model_list:
-            if isinstance(model, Map2DModel):
-                model.recalculate_vertices_from_grid_async(quality=self.__engine.get_quality(), then=then_routine)
-            else:
-                raise NotImplementedError("This type of model doesnt have a method to be reloaded.")
-
-    def remove_model(self, id_model: str) -> None:
-        """
-        Return the model with the specified id.
-        Args:
-            id_model: Id of the model to remove.
-
-        Returns: None
-        """
-        model_to_remove = None
-        for model in self.__model_list:
-            if model.id == id_model:
-                model_to_remove = model
-
-        self.__model_list.remove(model_to_remove)
-
-    def set_polygon_mode(self, polygon_mode: OGLConstant.IntConstant) -> None:
-        """
-        Select if the models uses the wireframe mode or not.
-        Args:
-            polygon_mode: OpenGL polygon mode to draw the model.
-
-        Returns: None
-        """
-        for model in self.__model_list:
-            model.polygon_mode = polygon_mode
-
-    def update_viewport(self) -> None:
-        """
-        Update the viewport with the new values that exist in the Settings.
-        """
-        log.debug("Updating viewport")
-        self.update_viewport_variables()
-
-        scene_data = self.__engine.get_scene_setting_data()
-        GL.glViewport(scene_data['SCENE_BEGIN_X'], scene_data['SCENE_BEGIN_Y'], scene_data['SCENE_WIDTH_X'],
-                      scene_data['SCENE_HEIGHT_Y'])
-
-        self.update_models_projection_matrix()
-
-    def update_models_projection_matrix(self) -> None:
-        """
-        Update the projection matrix of the models.
-
-        Returns: None
-        """
-        log.debug("Updating projection matrix of models.")
-        scene_data = self.__engine.get_scene_setting_data()
-
-        for model in self.__model_list:
-            if isinstance(model, Map2DModel):
-                model.calculate_projection_matrix(scene_data, self.__engine.get_zoom_level())
-            else:
-                raise NotImplementedError("Not implemented method to update the projection matrix in this model.")
-
-    def set_parallel_task(self, parallel_task, then):
-        """
-        Set a parallel task in the engine.
-
-        Args:
-            parallel_task: Task to execute in parallel
-            then: Task to execute after the parallel task
-
-        Returns: None
-        """
-        self.__engine.set_thread_task(parallel_task, then)
+        self.__engine = engine
 
     def refresh_with_model_2d_async(self, path_color_file: str, path_model: str, model_id: str = 'main',
                                     then=lambda: None) -> 'Model':
@@ -214,22 +121,77 @@ class Scene:
         log.debug("Setting vertices from grid.")
         model.set_vertices_from_grid_async(X, Y, Z, self.__engine.get_quality(), then_routine)
 
-    def get_float_bytes(self) -> int:
+    def reload_models_async(self, then):
         """
-        Get the float bytes used in a float to render.
-        Ask the engine for this information (that is stored in the settings).
+        Ask the 2D models to reload with the new resolution of the screen.
 
-        Returns: Number of bytes used for store float numbers.
-        """
-        return self.__engine.get_float_bytes()
+        Returns: None
 
-    def get_scene_setting_data(self) -> dict:
+        Args:
+            then: Function to be called after all the async routine.
         """
-        Get the scene settings.
+
+        # set up the then routine that should executes only once.
+        # -------------------------------------------------------
+        self.__should_execute_then_reload = len(self.__model_list)
+
+        def then_routine():
+            self.__should_execute_then_reload -= 1
+
+            if self.__should_execute_then_reload == 0:
+                self.__should_execute_then_reload = len(self.__model_list)
+                then()
+
+        for model in self.__model_list:
+            if isinstance(model, Map2DModel):
+                model.recalculate_vertices_from_grid_async(quality=self.__engine.get_quality(), then=then_routine)
+            else:
+                raise NotImplementedError("This type of model doesnt have a method to be reloaded.")
+
+    def remove_all_models(self) -> None:
+        """
+        Remove all models from the list of models.
+        Returns: None
+        """
+        self.__model_list = []
+
+    def remove_model(self, id_model: str) -> None:
+        """
+        Return the model with the specified id.
+        Args:
+            id_model: Id of the model to remove.
 
         Returns: None
         """
-        return self.__engine.get_scene_setting_data()
+        model_to_remove = None
+        for model in self.__model_list:
+            if model.id == id_model:
+                model_to_remove = model
+
+        self.__model_list.remove(model_to_remove)
+
+    def set_parallel_task(self, parallel_task, then):
+        """
+        Set a parallel task in the engine.
+
+        Args:
+            parallel_task: Task to execute in parallel
+            then: Task to execute after the parallel task
+
+        Returns: None
+        """
+        self.__engine.set_thread_task(parallel_task, then)
+
+    def set_polygon_mode(self, polygon_mode: OGLConstant.IntConstant) -> None:
+        """
+        Select if the models uses the wireframe mode or not.
+        Args:
+            polygon_mode: OpenGL polygon mode to draw the model.
+
+        Returns: None
+        """
+        for model in self.__model_list:
+            model.polygon_mode = polygon_mode
 
     def update_models_colors(self) -> None:
         """
@@ -240,3 +202,41 @@ class Scene:
         color_file = self.__engine.get_CPT_file()
         for model in self.__model_list:
             model.set_color_file(color_file)
+
+    def update_models_projection_matrix(self) -> None:
+        """
+        Update the projection matrix of the models.
+
+        Returns: None
+        """
+        log.debug("Updating projection matrix of models.")
+        scene_data = self.__engine.get_scene_setting_data()
+
+        for model in self.__model_list:
+            if isinstance(model, Map2DModel):
+                model.calculate_projection_matrix(scene_data, self.__engine.get_zoom_level())
+            else:
+                raise NotImplementedError("Not implemented method to update the projection matrix in this model.")
+
+    def update_viewport(self) -> None:
+        """
+        Update the viewport with the new values that exist in the Settings.
+        """
+        log.debug("Updating viewport")
+        self.update_viewport_variables()
+
+        scene_data = self.__engine.get_scene_setting_data()
+        GL.glViewport(scene_data['SCENE_BEGIN_X'], scene_data['SCENE_BEGIN_Y'], scene_data['SCENE_WIDTH_X'],
+                      scene_data['SCENE_HEIGHT_Y'])
+
+        self.update_models_projection_matrix()
+
+    def update_viewport_variables(self):
+        """
+        Update the viewport variables.
+
+        Returns: None
+        """
+        viewport_data = self.__engine.get_scene_setting_data()
+        self.__width_viewport = viewport_data['SCENE_WIDTH_X']
+        self.__height_viewport = viewport_data['SCENE_HEIGHT_Y']
