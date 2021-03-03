1eacdaec7a5815e1416601102e8b28a2ce85974a
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-03 17:39:23 -0300

    Updated how the expection are stored in the code

---
+++ src/error/repeated_point_error.py
@@ -0,0 +1,11 @@
+"""
+File that contain the repeatedPointError exception.
+"""
+from src.error.polygon_error import PolygonError
+
+
+class RepeatedPointError(PolygonError):
+    """
+    Class that represents when an error related to repeated points happens.
+    """
+    pass
