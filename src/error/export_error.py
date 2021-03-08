6597438f9f1451f1a216d5afc5adc9e4969dc96f
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-08 19:56:43 -0300

    Added class exportError to errors

---
+++ src/error/export_error.py
@@ -0,0 +1,11 @@
+"""
+Document with the class ExportError, class to use for error that happens during the export process
+"""
+
+
+class ExportError(Exception):
+    """
+    Base class to use for exportation errors.
+    """
+
+    pass
