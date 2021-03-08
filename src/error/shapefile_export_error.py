3f5059260fb2ab5797db73a98a3786d04b614410
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-08 20:00:18 -0300

    Added class SHapefileExportError to the error class hierarchy

---
+++ src/error/shapefile_export_error.py
@@ -0,0 +1,12 @@
+"""
+File that contains the class ShapefileExportError, class to use when there are errors when exporting shapefile files.
+"""
+
+from src.error.export_error import ExportError
+
+
+class ShapefileExportError(ExportError):
+    """
+    Class to use when there are errors in the export process of the shapefile files.
+    """
+    pass
