cdb231c691a54159c7dbcb3f448b875ae830d124
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-11 18:46:44 -0300

    Added behaviour of the polygon information frame forwhen the frames are fixed

--- src/engine/GUI/frames/polygon_information.py
+++ src/engine/GUI/frames/polygon_information.py
@@ -22,6 +22,8 @@ class PolygonInformation(Frame):
         Constructor of the class.
         """
         super().__init__(gui_manager)
+        self.__height = 300
+        self.__width = 200
 
     def render(self) -> None:
         """
@@ -44,4 +46,13 @@ class PolygonInformation(Frame):
         imgui.separator()
 
         imgui.columns(1)
+
+        if self._GUI_manager.are_frame_fixed():
+            self.change_position([self._GUI_manager.get_window_width() - self.__width,
+                                  self._GUI_manager.get_window_height() - self.__height])
+            imgui.set_window_position(self.get_position()[0], self.get_position()[1])
+            imgui.set_window_size(self.__width,
+                                  self.__height,
+                                  0)
+
         imgui.end()
