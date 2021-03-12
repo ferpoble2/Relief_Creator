272c1ed245dfbd5ec3c6c62cadc761ccc4506bb6
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-12 12:00:42 -0300

    Firmatted filed

--- src/engine/GUI/frames/polygon_information.py
+++ src/engine/GUI/frames/polygon_information.py
@@ -35,13 +35,11 @@ class PolygonInformation(Frame):
 
             # set the flags if the windows should be collapsable or not
             if self._GUI_manager.are_frame_fixed():
-                imgui.begin('Polygon Information',False, imgui.WINDOW_NO_MOVE | imgui.WINDOW_NO_COLLAPSE)
+                imgui.begin('Polygon Information', False, imgui.WINDOW_NO_MOVE | imgui.WINDOW_NO_COLLAPSE)
                 self.change_position([self._GUI_manager.get_window_width() - self.__width,
                                       self._GUI_manager.get_window_height() - self.__height])
                 imgui.set_window_position(self.get_position()[0], self.get_position()[1])
-                imgui.set_window_size(self.__width,
-                                      self.__height,
-                                      0)
+                imgui.set_window_size(self.__width, self.__height, 0)
 
             else:
                 imgui.begin('Polygon Information')
