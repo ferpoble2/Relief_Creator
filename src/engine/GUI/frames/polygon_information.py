8b76d6b8e156607ebfe4702f33b063c87abfad3d
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-15 12:20:39 -0300

    Added popup to add a new parameter

--- src/engine/GUI/frames/polygon_information.py
+++ src/engine/GUI/frames/polygon_information.py
@@ -25,6 +25,10 @@ class PolygonInformation(Frame):
         self.__height = 300
         self.__width = 200
 
+        # auxiliary variables
+        self.__key_string_value = 'Name of parameter'
+        self.__value_string_value = 'Value of parameter'
+
     def render(self) -> None:
         """
         Render the frame.
@@ -45,12 +49,14 @@ class PolygonInformation(Frame):
             else:
                 imgui.begin('Polygon Information')
 
+            self._GUI_manager.set_bold_font()
             imgui.columns(2, 'Data List')
             imgui.separator()
             imgui.text("Field Name")
             imgui.next_column()
             imgui.text("Value")
             imgui.separator()
+            self._GUI_manager.set_regular_font()
 
             for parameter in self._GUI_manager.get_polygon_parameters(self._GUI_manager.get_active_polygon_id()):
                 imgui.next_column()
@@ -60,5 +66,31 @@ class PolygonInformation(Frame):
                 imgui.separator()
 
             imgui.columns(1)
+
+            if imgui.button("Add new", -1):
+                imgui.open_popup('Add new parameter')
+
+            # popup modal
+            imgui.set_next_window_size(-1, -1)
+            if imgui.begin_popup_modal('Add new parameter')[0]:
+                _, self.__key_string_value = imgui.input_text('Name of the parameter:',
+                                                              self.__key_string_value,
+                                                              20)
+                _, self.__value_string_value = imgui.input_text('Value of the parameter:',
+                                                                self.__value_string_value,
+                                                                50)
+                if imgui.button('Done', -1):
+
+                    # set the new parameter
+                    self._GUI_manager.set_polygon_parameter(self._GUI_manager.get_active_polygon_id(),
+                                                            self.__key_string_value,
+                                                            self.__value_string_value)
+
+                    # reset the variables
+                    self.__key_string_value = 'Name of parameter'
+                    self.__value_string_value = 'Value of parameter'
+                    imgui.close_current_popup()
+
+                imgui.end_popup()
 
             imgui.end()
