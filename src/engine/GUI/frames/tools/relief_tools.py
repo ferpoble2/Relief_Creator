95a7586f13fc3cc30438443b734b4c2154286395
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-19 13:19:58 -0300

    Changed name of transformation to linear

--- src/engine/GUI/frames/tools/relief_tools.py
+++ src/engine/GUI/frames/tools/relief_tools.py
@@ -35,7 +35,7 @@ class ReliefTools:
         imgui.text('Relief Tools')
 
         clicked, self.__current_combo_option = imgui.combo(
-            "Type of interpolation", self.__current_combo_option, self.__combo_options
+            "Transformation", self.__current_combo_option, self.__combo_options
         )
 
         imgui.input_float('Min Height', self.__min_height_value)
@@ -43,8 +43,8 @@ class ReliefTools:
 
         if imgui.button('Change Height', -1):
             if self.__current_combo_option == 0:
-                self.__gui_manager.change_height_using_active_polygon(self.__gui_manager.get_active_polygon_id(),
-                                                                      self.__gui_manager.get_active_model_id(),
-                                                                      min_height=self.__min_height_value,
-                                                                      max_height=self.__max_height_value,
-                                                                      interpolation_type='linear')
+                self.__gui_manager.change_points_height(self.__gui_manager.get_active_polygon_id(),
+                                                        self.__gui_manager.get_active_model_id(),
+                                                        min_height=self.__min_height_value,
+                                                        max_height=self.__max_height_value,
+                                                        interpolation_type='linear')
