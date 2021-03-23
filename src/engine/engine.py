8a5a551074f99adc3e939a75b1e1c15ee95c42f3
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-23 12:05:54 -0300

    Added function to set the program in a loading state.
    Added logic to use parameters with the thread tasks

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -717,25 +717,44 @@ class Engine:
 
         self.set_task_for_next_frame(task_loading)
 
-    def set_thread_task(self, parallel_task, then) -> None:
+    def set_program_loading(self, new_state: bool = True) -> None:
+        """
+        Tell the program to set the loading state.
+
+        Returns: None
+
+        Args:
+            new_state: Boolean indicating the state of the program (if it is loading or not)
+        """
+        self.program.set_loading(new_state)
+
+    def set_thread_task(self, parallel_task, then, parallel_task_args=None, then_task_args=None) -> None:
         """
         Add and start a new thread with the current task. At the end of the thread, the then
         function is called.
 
         Args:
+            then_task_args: List of argument to use in the then task
+            parallel_task_args: List of argument to use in the parallel task
             parallel_task: Task to be executed in parallel
             then: Task to be executed in the main thread after the parallel task
 
         Returns: None
         """
         # Create and start the thread
-        thread = Thread(target=parallel_task)
+        if then_task_args is None:
+            then_task_args = []
+        if parallel_task_args is None:
+            parallel_task_args = []
+
+        thread = Thread(target=parallel_task, args=parallel_task_args)
         thread.start()
 
         # Add thread to the list
         self.__threads_list.append(
             {'thread': thread,
-             'then_func': then}
+             'then_func': then,
+             'then_args': then_task_args}
         )
 
     def undo_action(self) -> None:
@@ -832,7 +851,7 @@ class Engine:
         for thread_pair in self.__threads_list:
             if not thread_pair['thread'].is_alive():
                 to_delete.append(thread_pair)
-                thread_pair['then_func']()
+                thread_pair['then_func'](*thread_pair['then_args'])
 
         for thread_ended in to_delete:
             log.debug("Thread ended")
