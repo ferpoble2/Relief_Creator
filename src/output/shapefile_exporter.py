40c6b09aca77cb93658b82c44a2cfb291a46391e
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-29 12:43:41 -0300

    Changed how the polygons ar reverted when they are defined in the other way

--- src/output/shapefile_exporter.py
+++ src/output/shapefile_exporter.py
@@ -73,14 +73,8 @@ class ShapefileExporter:
 
             # sort the points to be counter clockwise
             points = self.__delete_z_axis(list_of_points[ind])
-            if is_clockwise(points):  # polygons must be defined CCW
-                new_points = []
-                for point_ind in range(int(len(points) / 2)):
-                    last_y = points.pop()
-                    last_x = points.pop()
-                    new_points.append(last_x)
-                    new_points.append(last_y)
-                points = new_points
+            if is_clockwise(points):
+                points.reverse()  # polygons must be defined CCW
 
             processed_point_list.append(points)
 
@@ -165,14 +159,8 @@ class ShapefileExporter:
 
         # Save the polygons
         points = self.__delete_z_axis(list_of_points)
-        if is_clockwise(points):  # polygons must be defined CCW
-            new_points = []
-            for point_ind in range(int(len(points) / 2)):
-                last_y = points.pop()
-                last_x = points.pop()
-                new_points.append(last_x)
-                new_points.append(last_y)
-            points = new_points
+        if is_clockwise(points):
+            points.reverse()  # polygons must be defined CCW
 
         params = list(parameters.values())
         w.record(*params)
