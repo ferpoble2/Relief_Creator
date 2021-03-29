733bc60171c58b929e73c078081e06e5f1a1f65e
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-29 12:26:33 -0300

    Changed functionality of is_clockwise to utils module

--- src/utils.py
+++ src/utils.py
@@ -89,3 +89,21 @@ def is_numeric(value: str) -> bool:
         return True
     except ValueError:
         return False
+
+
+def is_clockwise(points):
+    """
+    Check if a list of 2D points are in CW order or not.
+
+    Args:
+        points: List of 2D points [(1.x,1.y),(2.x,2.y),...]
+
+    Returns: Boolean indicating if points are CW or not.
+    """
+
+    # points is your list (or array) of 2d points.
+    assert len(points) > 0
+    s = 0.0
+    for p1, p2 in zip(points, points[1:] + [points[0]]):
+        s += (p2[0] - p1[0]) * (p2[1] + p1[1])
+    return s > 0.0
