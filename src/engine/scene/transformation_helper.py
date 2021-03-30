8047d6b53f73b5a17937c761f27ce356479b9148
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-30 16:27:38 -0300

    Added interpolate.scipy to the dependencies of transformation helper
    Added nan_mask as parameter to the __interpolate_nan method.
    
    Implemented __interpolate_nan method.
    
    Added __get_bounding_box_method

--- src/engine/scene/transformation_helper.py
+++ src/engine/scene/transformation_helper.py
@@ -5,6 +5,7 @@ import numpy as np
 from shapely.geometry.polygon import LinearRing as LinearRing
 from typing import List
 from matplotlib import path
+from scipy import interpolate as interpolate_scipy
 
 from src.utils import interpolate
 from src.utils import is_clockwise
@@ -49,14 +50,14 @@ class TransformationHelper:
                 pair_used = []
         return new_list
 
-    def __generate_mask(self, points, polygon_points) -> np.ndarray:
+    def __generate_mask(self, points_array: np.ndarray, polygon_points) -> np.ndarray:
         """
         Generate a mask of the points that are inside the specified polygon.
 
         Mask is a numpy array with booleans representing if the point is inside the polygon or not.
 
         Args:
-            points: Numpy array with the points (shape must be (x, y, 3))
+            points_array: Numpy array with the points (shape must be (x, y, 3))
             polygon_points: List with the points of the polygon. [x1, y1, z1, x2, y2, z2, ...]
 
         Returns: Numpy array with booleans indicating if the points are inside the polygon or not.
@@ -68,8 +69,9 @@ class TransformationHelper:
             if point_ind % 3 == 0:
                 points_xy.append((polygon_points[point_ind], polygon_points[point_ind + 1]))
         # get a mask for the points inside
-        xv = points[:, :, 0]
-        yv = points[:, :, 1]
+        xv = points_array[:, :, 0]
+        yv = points_array[:, :, 1]
+        # noinspection PyTypeChecker
         p = path.Path(points_xy)
 
         # noinspection PyTypeChecker
@@ -78,25 +80,49 @@ class TransformationHelper:
         return flags
 
     # noinspection PyShadowingNames
-    def __interpolate_nan(self, array_2d: np.ndarray, interpolation_type: str = 'linear') -> np.ndarray:
+    def __interpolate_nan(self, array_2d: np.ndarray, nan_mask: np.ndarray,
+                          interpolation_type: str = 'linear') -> np.ndarray:
         """
         Interpolate the missing values from the array2d using scipy interpolation method.
 
         Args:
             array_2d: Array 2D with missing values to interpolate.
+            nan_mask: Array 2D with a mask specifying where are located the nan values.
             interpolation_type: Type of the interpolation. (nearest, linear, cubic)
 
         Returns: Array interpolated.
         """
-        raise NotImplementedError()
+        log.debug('Interpolating nan')
 
-        # TODO: implement a fast way of interpolating:
-        #  help links:
-        #  https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.interp2d.html
-        #  https://docs.scipy.org/doc/scipy/reference/generated/scipy.interpolate.RectBivariateSpline.html#scipy.interpolate.RectBivariateSpline
-        #  or any other thing that can help from the gmt library
+        log.debug('Generating data arrays')
+        grid_x, grid_y = np.mgrid[:array_2d.shape[0], :array_2d.shape[1]]
+        data = np.zeros((array_2d.shape[0], array_2d.shape[1], 3))
+        data[:, :, 0] = grid_x
+        data[:, :, 1] = grid_y
+        data[:, :, 2] = array_2d
 
-    def modify_points_inside_polygon_linear(self, points: np.ndarray,
+        nan_mask = nan_mask.reshape(-1)
+        data = data.reshape((-1, 3))
+
+        points = data[~nan_mask][:, 0:2]
+        values = data[~nan_mask][:, 2]
+
+        points_to_interpolate = data[nan_mask][:, 0:2]
+
+        log.debug('Initiated interpolate process using numpy')
+        y = interpolate_scipy.griddata(points, values, points_to_interpolate, method=interpolation_type)
+        log.debug('Finished interpolation')
+
+        data_nan_mask = data[nan_mask]
+        data_nan_mask[:, 2] = y
+        data[nan_mask] = data_nan_mask
+        data = data[:, 2]
+        data = data.reshape(array_2d.shape)
+        log.debug('Ended nan interpolation')
+
+        return data
+
+    def modify_points_inside_polygon_linear(self, points_array: np.ndarray,
                                             height: np.ndarray,
                                             polygon_points: List[float],
                                             new_max_height: float,
@@ -107,14 +133,14 @@ class TransformationHelper:
 
         Args:
             height: Array with the height of the points. must have shape (x, y)
-            points: Points to modify (numpy array of points), must have shape (x, y, 3)
+            points_array: Points to modify (numpy array of points), must have shape (x, y, 3)
             polygon_points: List of points in the polygon. [x1, y1, z1, x2, y2, z2, ...]
             new_max_height: New height to interpolate the points
             new_min_height: New height to interpolate the points
 
         Returns: numpy.array with the new points
         """
-        flags = self.__generate_mask(points, polygon_points)
+        flags = self.__generate_mask(points_array, polygon_points)
 
         # modify the height linearly
         current_min_height = np.min(height[flags])
@@ -126,14 +152,37 @@ class TransformationHelper:
         height[flags] = new_height
         return height
 
-    def interpolate_points_external_to_polygon(self, points: np.ndarray, polygon_points: list,
+    # noinspection PyUnresolvedReferences
+    def __get_bounding_box_indexes(self, points_array: np.ndarray, polygon: LinearRing) -> list:
+        """
+        Get the indices to use in the matrix to get only the values of the matrix that are inside the bounding
+        box of the polygon.
+
+        Args:
+            points_array: Array 2D with shape (x,y,3) with the values of the points.
+            polygon: Polygon to use to get the bounding box.
+
+        Returns: [min_x, max_x, min_y, max_y]
+        """
+
+        # get the bounding box of the nan values
+        (minx, miny, max_x, maxy) = polygon.bounds
+        min_x_index = np.searchsorted(points_array[0, :, 0], minx)
+        max_x_index = np.searchsorted(points_array[0, :, 0], max_x)
+
+        min_y_index = np.searchsorted(points_array[:, 0, 1], miny)
+        max_y_index = np.searchsorted(points_array[:, 0, 1], maxy)
+
+        return [min_x_index, max_x_index, min_y_index, max_y_index]
+
+    def interpolate_points_external_to_polygon(self, points_array: np.ndarray, polygon_points: list,
                                                heights: np.ndarray, distance: float) -> np.ndarray:
         """
         Interpolate the points that are external to the polygon until a certain distance.
 
         Args:
             distance: Limit distance to interpolate the points from.
-            points: Points of the model. (shape must be (x, y, 3))
+            points_array: Points of the model. (shape must be (x, y, 3))
             polygon_points: List with the points of the polygon. [x1, y1, z1, x2, y2, z2, ...]
             heights: Array with the height of the points. must have shape (x, y)
 
@@ -159,39 +208,42 @@ class TransformationHelper:
             polygon_exterior.append(y_coordinate)
             polygon_exterior.append(0.5)
 
+        # get the bounding box of the nan values
+        min_x_index, max_x_index, min_y_index, max_y_index = self.__get_bounding_box_indexes(points_array,
+                                                                                             exterior_polygon)
+
+        points_cut = points_array[min_y_index:max_y_index, min_x_index:max_x_index, :]
+        heights_cut = heights[min_y_index:max_y_index, min_x_index:max_x_index]
+
         # generate a mask of the internal points
         log.debug('Generating masks')
-        mask_external = self.__generate_mask(points, polygon_exterior)
-        mask_internal = self.__generate_mask(points, polygon_points)
+        mask_external = self.__generate_mask(points_cut, polygon_exterior)
+        mask_internal = self.__generate_mask(points_cut, polygon_points)
 
         # apply the logical operations
         in_between_mask = mask_external != mask_internal
-        heights[in_between_mask == True] = np.nan
+        heights_cut[in_between_mask == True] = np.nan # noqa
 
         # apply the interpolation algorithm from scipy
         log.debug('Interpolating using numpy')
-        heights = self.__interpolate_nan(heights)
-
-        # TODO: delete this after the code for interpolate is complete
-        log.debug('Plotting')
-        import matplotlib.pyplot as plot
-        plot.imshow(heights)
-        plot.show()
+        heights_cut = self.__interpolate_nan(heights_cut, in_between_mask)
+        heights[min_y_index:max_y_index, min_x_index:max_x_index] = heights_cut
 
         return heights
 
-    def get_max_min_inside_polygon(self, points: np.ndarray, polygon_points: List[float], heights: np.ndarray) -> tuple:
+    def get_max_min_inside_polygon(self, points_array: np.ndarray, polygon_points: List[float],
+                                   heights: np.ndarray) -> tuple:
         """
         Extract the maximum and minimum value of the points that are inside the polygon.
 
         Args:
-            points: Points of the model. (shape must be (x, y, 3))
+            points_array: Points of the model. (shape must be (x, y, 3))
             polygon_points: List with the points of the polygon. [x1, y1, z1, x2, y2, z2, ...]
             heights: height: Array with the height of the points. must have shape (x, y)
 
         Returns: Tuple with the maximum and minimum value
         """
-        flags = self.__generate_mask(points, polygon_points)
+        flags = self.__generate_mask(points_array, polygon_points)
 
         maximum = np.max(heights[flags])
         minimum = np.min(heights[flags])
@@ -229,26 +281,86 @@ if __name__ == '__main__':
     from scipy import interpolate
     import matplotlib.pyplot as plt
 
+    #
     # Let's create some random  data
-    array = np.random.random_integers(0, 10, (10, 10)).astype(float)
-    # values grater then 7 goes to np.nan
-    array[2:10, 2:10] = np.nan
+    array = np.random.random_integers(0, 10, (100, 100)).astype(float)
+    array[:, :] = 10
+    array[0:10, :] = 100
+    array[:, 0:10] = 100
+    array[10:70, 10:70] = np.nan
 
-    x = np.arange(0, array.shape[1])
-    y = np.arange(0, array.shape[0])
-    # mask invalid values
-    array = np.ma.masked_invalid(array)
-    xx, yy = np.meshgrid(x, y)
-    # get only the valid values
-    x1 = xx[~array.mask]
-    y1 = yy[~array.mask]
-    new_array = array[~array.mask]
+    array[80, 80:90] = np.nan
+    array[80:90, 90] = np.nan
+    plt.imshow(array)
+    plt.show()
 
-    GD1 = interpolate.griddata((x1, y1), new_array.ravel(),
-                               (xx, yy),
-                               method='linear')
+    # x = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    # y = np.array([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    #
+    # GD1 = interpolate.interp2d(x, y, array)
+    #
+    # Z = GD1(y, x)
+
+    # def nan_helper(y):
+    #     """Helper to handle indices and logical indices of NaNs.
+    #
+    #     Input:
+    #         - y, 1d numpy array with possible NaNs
+    #     Output:
+    #         - nans, logical indices of NaNs
+    #         - index, a function, with signature indices= index(logical_indices),
+    #           to convert logical indices of NaNs to 'equivalent' indices
+    #     Example:
+    #         >>> # linear interpolation of NaNs
+    #         >>> nans, x= nan_helper(y)
+    #         >>> y[nans]= np.interp(x(nans), x(~nans), y[~nans])
+    #     """
+    #
+    #     return np.isnan(y), lambda z: z.nonzero()[0]
+    #
+    # y = array
+    # nans, x = nan_helper(y)
+    # y[nans] = np.interp(x(nans), x(~nans), y[~nans])
+
+    grid_x, grid_y = np.mgrid[10:70, 10:70]
+    points = []
+    values = []
+
+    for row in range(len(array)):
+        for col in range(len(array[0])):
+            if not np.isnan(array[row][col]):
+                points.append([col, row])
+                values.append(array[row][col])
+
+    interpolate_this = [
+        [80, 80],
+        [80, 81],
+        [80, 82],
+        [80, 83],
+        [80, 84],
+        [80, 85],
+        [80, 86],
+        [80, 87],
+        [80, 88],
+        [80, 89],
+        [80, 90],
+        [81, 90],
+        [82, 90],
+        [83, 90],
+        [84, 90],
+        [85, 90],
+        [86, 90],
+        [87, 90],
+        [88, 90],
+        [89, 90],
+    ]
+
+    # noinspection PyTypeChecker
+    y = interpolate.griddata(points, values, interpolate_this, method='cubic')
+    print(y)
+    array[10:70, 10:70] = y
 
     plt.imshow(array)
     plt.show()
-    plt.imshow(GD1)
+    plt.imshow(Z)
     plt.show()
