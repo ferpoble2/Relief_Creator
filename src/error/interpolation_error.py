5e7c8785a3330096f4db59c8c2379c810a45d4d3
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-03-31 17:19:13 -0300

    Created Interpolation Error class

---
+++ src/error/interpolation_error.py
@@ -0,0 +1,30 @@
+"""
+File with the InterpolationError class, class to use when raising exceptions related to the interpolation process.
+"""
+
+from src.error.scene_error import SceneError
+
+
+class InterpolationError(SceneError):
+    """
+    Class to use when there is an error in the transformation process of a model.
+    """
+
+    def __init__(self, code: int = 0):
+        """
+        Constructor of the class.
+        """
+        self.code = code
+
+        self.__codes = {
+            0: 'Error description not selected.',
+            1: 'Not enough points in the polygon to do the interpolation.',
+        }
+
+    def get_code_message(self) -> str:
+        """
+        Get the message stored describing the error.
+
+        Returns: string
+        """
+        return self.__codes[self.code]
