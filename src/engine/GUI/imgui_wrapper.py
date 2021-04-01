88083d9b67132e566c62af5f5e20e0266e25b7fe
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-04-01 13:10:20 -0300

    changed imgui_wrapper to a module

--- src/engine/GUI/imgui_wrapper.py
+++ src/engine/GUI/imgui_wrapper.py
@@ -2,42 +2,32 @@
 File with the class ImguiWrapper, class that act as a wrapper for the imgui functions.
 """
 
-import imgui
+from imgui import *
 
 
-class ImguiWrapper:
+def close_current_popup_modal(gui_manager) -> None:
     """
-    Class that act as a wrapper for the imgui functionality.
+    Close the current popup modal and set all the configuration necessary to continue with the program.
+
+    Args:
+        gui_manager: Gui manager being used by the program.
+
+    Returns:
+
     """
+    gui_manager.enable_glfw_keyboard_callback()
+    close_current_popup()
 
-    # noinspection PyUnresolvedReferences
-    def __init__(self, gui_manager: 'GUIManager'):
-        """
-        Constructor of the class.
 
-        Args:
-            gui_manager: gui_manager to use.
-        """
-        self.__gui_manager = gui_manager
+def open_popup_modal(gui_manager, popup_id: str) -> None:
+    """
+    Open the popup modal with the given name and set all the configuration previous too open it.
 
-    def close_current_popup_modal(self) -> None:
-        """
-        Close the current popup modal and set all the configuration necessary to continue with the program.
+    Args:
+        gui_manager: Gui manager being used by the program.
+        popup_id: Popup ID
 
-        Returns:
-
-        """
-        self.__gui_manager.enable_glfw_keyboard_callback()
-        imgui.close_current_popup()
-
-    def open_popup_modal(self, popup_id: str) -> None:
-        """
-        Open the popup modal with the given name and set all the configuration previous too open it.
-
-        Args:
-            popup_id: Popup ID
-
-        Returns: None
-        """
-        self.__gui_manager.disable_glfw_keyboard_callback()
-        imgui.open_popup(popup_id)
+    Returns: None
+    """
+    gui_manager.disable_glfw_keyboard_callback()
+    open_popup(popup_id)
