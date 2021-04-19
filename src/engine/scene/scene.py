8347618f0619f9fb085da4ee12516af3847ecdfd
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-04-19 10:43:52 -0400

    Fixed bug where the interpolation area remains after deleting the polygon.

--- src/engine/scene/scene.py
+++ src/engine/scene/scene.py
@@ -340,6 +340,10 @@ class Scene:
         if polygon_id in self.__polygon_hash:
             self.__polygon_hash.pop(polygon_id)
 
+            # delete the interpolation area if they have
+            if polygon_id in self.__interpolation_area_hash:
+                self.__interpolation_area_hash.pop(polygon_id)
+
     def delete_polygon_param(self, polygon_id: str, key: str) -> None:
         """
         Delete a parameter from the polygon.
