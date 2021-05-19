f6b5856df9bf95cc4bc14cb1204c5b4de3371c8d
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-05-19 11:56:43 -0400

    Added codes to the guierror class

--- src/error/GUI_error.py
+++ src/error/GUI_error.py
@@ -8,4 +8,14 @@ class GUIError(BaseError):
     """
     Error to use for GUI related errors.
     """
-    pass
+
+    def __init__(self, code=0):
+        """
+        Constructor of the class.
+        """
+        super(GUIError, self).__init__(code=code)
+
+        self.codes = {
+            0: 'Default Error.',
+            1: 'Filter ID not found on the table of filters.'
+        }
