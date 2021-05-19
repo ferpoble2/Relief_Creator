42c7333836160e25d5775e486eac848b2774357a
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-05-19 15:57:54 -0400

    changed error message code

--- src/error/model_transformation_error.py
+++ src/error/model_transformation_error.py
@@ -23,5 +23,5 @@ class ModelTransformationError(SceneError):
             3: 'Polygon used is not planar.',
             4: 'Can not use that model for transforming points. Try using a Map2DModel.',
             5: f'Can not get data of filter. Unrecognized filter {filter_name}',
-            6: 'Polygon not found or selected.'
+            6: 'Polygon to use in a filter not found or selected.'
         }
