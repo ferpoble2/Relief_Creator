62e956d5b44b40a689c6f1f7ac7dc6efcb01f9bc
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-05-20 14:29:21 -0400

    Changed location of tests

--- test/input/test_import_CPT.py
+++ test/input/test_import_CPT.py
@@ -16,14 +16,14 @@ class TestReadCPTFile(unittest.TestCase):
     def test_reading_normal_files(self):
         warnings.simplefilter("ignore", ResourceWarning)
 
-        with open(os.path.join(FILES_DIRECTORY, 'test_data_CPT_1.json')) as f:
+        with open(os.path.join(FILES_DIRECTORY, 'cpt/test_data_CPT_1.json')) as f:
             data_1 = json.load(f)
-            data_read = read_file(os.path.join(FILES_DIRECTORY, 'test_cpt_1.cpt'))
+            data_read = read_file(os.path.join(FILES_DIRECTORY, 'cpt/test_cpt_1.cpt'))
             self.assertEqual(data_1, data_read, 'Data read from CPT file is not what is expected.')
 
-        with open(os.path.join(FILES_DIRECTORY, 'test_data_CPT_2.json')) as f:
+        with open(os.path.join(FILES_DIRECTORY, 'cpt/test_data_CPT_2.json')) as f:
             data_2 = json.load(f)
-            data_read = read_file(os.path.join(FILES_DIRECTORY, 'test_cpt_2.cpt'))
+            data_read = read_file(os.path.join(FILES_DIRECTORY, 'cpt/test_cpt_2.cpt'))
             self.assertEqual(data_2, data_read, 'Data read from CPT file is not what is expected.')
 
 
