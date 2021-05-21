7814a10434bb594664298bb58562605fa7e29814
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-05-21 17:51:16 -0400

    Created test for the filter functionality.

---
+++ test/filters/test_height_filter.py
@@ -0,0 +1,65 @@
+"""
+File with the tests related to the functionality that applies height filters to the polygons.
+"""
+import unittest
+import warnings
+import os
+
+from src.engine.engine import Engine
+from src.program.program import Program
+from src.input.NetCDF import read_info
+
+FILES_DIRECTORY = './test/filters/files/'
+
+
+class TestHeightGreaterFilter(unittest.TestCase):
+
+    def setUp(self) -> None:
+        """
+        Code executed before every test. Initializes a program to work with.
+        """
+
+        # create program
+        self.engine = Engine()
+        self.program = Program(self.engine)
+
+        # initialize variables
+        self.engine.should_use_threads(False)
+
+    # TODO: Test case when polygon does not use all the map
+    # TODO: Test case when polygon is outside of map
+
+    def test_normal_application(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+
+        self.engine.refresh_with_model_2d(os.path.join(FILES_DIRECTORY, 'colors.cpt'),
+                                          os.path.join(FILES_DIRECTORY, 'netcdf', 'test_file_50_50.nc'))
+
+        # load polygon
+        self.engine.load_polygon_from_shapefile(os.path.join(FILES_DIRECTORY, 'polygons', 'filter_1'))
+
+        # create polygon to modify the scene.
+        self.engine.transform_points(polygon_id=self.engine.get_active_polygon_id(),
+                                     model_id=self.engine.get_active_model_id(),
+                                     min_height=100,
+                                     max_height=200,
+                                     transformation_type='linear',
+                                     filters=[('height_greater_than', 50)])
+
+        # export model to compare data
+        self.engine.export_model_as_netcdf(self.engine.get_active_model_id(), os.path.join(FILES_DIRECTORY,
+                                                                                           'netcdf',
+                                                                                           'temp_filter_1'))
+
+        # read data and compare
+        info_written = read_info(os.path.join(FILES_DIRECTORY, 'netcdf', 'temp_filter_1.nc'))
+        info_expected = read_info(os.path.join(FILES_DIRECTORY, 'netcdf', 'temp_filter_1.nc'))
+
+        self.assertTrue((info_written[0] == info_expected[0]).all())
+        self.assertTrue((info_written[1] == info_expected[1]).all())
+        self.assertTrue((info_written[2] == info_expected[2]).all())
+
+
+
+if __name__ == '__main__':
+    unittest.main()
