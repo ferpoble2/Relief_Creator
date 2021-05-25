0028236027497c7a286a0bafa86d9314321a52e4
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-05-25 17:28:25 -0400

    Completed filters for the normal functionality of the heights and added tests for the normal functionality of the contains filters.

--- test/filters/test_contain_filter.py
+++ test/filters/test_contain_filter.py
@@ -16,8 +16,9 @@
 #  END GPL LICENSE BLOCK
 
 """
-File with the tests related to the functionality that applies height filters to the polygons.
+File with the tests related to the contain filter functionality.
 """
+
 import unittest
 import warnings
 import os
@@ -27,7 +28,7 @@ from src.program.program import Program
 from src.input.NetCDF import read_info
 
 
-class TestHeightGreaterFilter(unittest.TestCase):
+class TestContainsFilter(unittest.TestCase):
 
     def setUp(self) -> None:
         """
@@ -41,67 +42,72 @@ class TestHeightGreaterFilter(unittest.TestCase):
         # initialize variables
         self.engine.should_use_threads(False)
 
-    def test_normal_application_less_than(self):
+    def test_normal_application_is_not_in(self):
         warnings.simplefilter("ignore", ResourceWarning)
 
         self.engine.refresh_with_model_2d('resources/test_resources/cpt/colors_0_100_200.cpt',
                                           'resources/test_resources/netcdf/test_file_50_50.nc')
 
-        # load polygon
-        self.engine.load_polygon_from_shapefile('resources/test_resources/polygons/shape_one_polygon_2.shp')
 
-        # create polygon to modify the scene.
-        self.engine.transform_points(polygon_id=self.engine.get_active_polygon_id(),
+        # load list of polygons
+        self.engine.load_polygon_from_shapefile('resources/test_resources/polygons/shape_two_concentric_polygons.shp')
+
+        # apply transformation with filters
+        self.engine.transform_points(polygon_id='Polygon 0',
                                      model_id=self.engine.get_active_model_id(),
                                      min_height=100,
                                      max_height=200,
                                      transformation_type='linear',
-                                     filters=[('height_less_than', 50)])
+                                     filters=[('is_not_in', 'Polygon 1')])
 
         # export model to compare data
         self.engine.export_model_as_netcdf(self.engine.get_active_model_id(),
-                                           'resources/test_resources/temp/temp_filter_3')
+                                           'resources/test_resources/temp/temp_filter_6')
 
         # read data and compare
-        info_written = read_info('resources/test_resources/temp/temp_filter_3.nc')
-        info_expected = read_info('resources/test_resources/expected_data/netcdf/expected_filter_3.nc')
+        info_written = read_info('resources/test_resources/temp/temp_filter_6.nc')
+        info_expected = read_info('resources/test_resources/expected_data/netcdf/expected_filter_6.nc')
 
         self.assertTrue((info_written[0] == info_expected[0]).all())
         self.assertTrue((info_written[1] == info_expected[1]).all())
         self.assertTrue((info_written[2] == info_expected[2]).all())
 
-        os.remove('resources/test_resources/temp/temp_filter_3.nc')
+        os.remove('resources/test_resources/temp/temp_filter_6.nc')
 
-    def test_normal_application_greater_than(self):
+    def test_normal_application_is_in(self):
         warnings.simplefilter("ignore", ResourceWarning)
 
         self.engine.refresh_with_model_2d('resources/test_resources/cpt/colors_0_100_200.cpt',
                                           'resources/test_resources/netcdf/test_file_50_50.nc')
 
-        # load polygon
-        self.engine.load_polygon_from_shapefile('resources/test_resources/polygons/shape_one_polygon_2.shp')
 
-        # create polygon to modify the scene.
-        self.engine.transform_points(polygon_id=self.engine.get_active_polygon_id(),
+        # load list of polygons
+        self.engine.load_polygon_from_shapefile('resources/test_resources/polygons/shape_three_concentric_polygons.shp')
+        print(self.engine.get_polygon_id_list())
+
+        # apply transformation with filters
+        self.engine.transform_points(polygon_id='Polygon 0',
                                      model_id=self.engine.get_active_model_id(),
                                      min_height=100,
                                      max_height=200,
                                      transformation_type='linear',
-                                     filters=[('height_greater_than', 50)])
+                                     filters=[('is_not_in', 'Polygon 1'),
+                                              ('is_in', 'Polygon 2')])
 
         # export model to compare data
         self.engine.export_model_as_netcdf(self.engine.get_active_model_id(),
-                                           'resources/test_resources/temp/temp_filter_4')
+                                           'resources/test_resources/temp/temp_filter_5')
 
         # read data and compare
-        info_written = read_info('resources/test_resources/temp/temp_filter_4.nc')
-        info_expected = read_info('resources/test_resources/expected_data/netcdf/expected_filter_4.nc')
+        info_written = read_info('resources/test_resources/temp/temp_filter_5.nc')
+        info_expected = read_info('resources/test_resources/expected_data/netcdf/expected_filter_5.nc')
 
         self.assertTrue((info_written[0] == info_expected[0]).all())
         self.assertTrue((info_written[1] == info_expected[1]).all())
         self.assertTrue((info_written[2] == info_expected[2]).all())
 
-        os.remove('resources/test_resources/temp/temp_filter_4.nc')
+        os.remove('resources/test_resources/temp/temp_filter_5.nc')
+
 
 if __name__ == '__main__':
     unittest.main()
