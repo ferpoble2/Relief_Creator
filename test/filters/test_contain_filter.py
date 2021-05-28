f084567eb26e315f6475013ba08ebc6cc855ae3f
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-05-28 16:19:09 -0400

    Added tests for more complicated logic of the filters

--- test/filters/test_contain_filter.py
+++ test/filters/test_contain_filter.py
@@ -105,6 +105,71 @@ class TestContainsFilter(unittest.TestCase):
 
         os.remove('resources/test_resources/temp/temp_filter_5.nc')
 
+    def test_polygon_not_in_map_is_in_filter(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+
+        self.engine.refresh_with_model_2d('resources/test_resources/cpt/colors_0_100_200.cpt',
+                                          'resources/test_resources/netcdf/test_file_50_50.nc')
+
+        # load list of polygons
+        self.engine.load_polygon_from_shapefile('resources/test_resources/polygons/'
+                                                'shape_three_polygons_south_america.shp')
+
+        # apply transformation with filters
+        self.engine.transform_points(polygon_id='Polygon 0',
+                                     model_id=self.engine.get_active_model_id(),
+                                     min_height=100,
+                                     max_height=200,
+                                     transformation_type='linear',
+                                     filters=[('is_not_in', 'Polygon 1')])
+
+        # export model to compare data
+        self.engine.export_model_as_netcdf(self.engine.get_active_model_id(),
+                                           'resources/test_resources/temp/temp_is_in_filter_polygon_not_in_map')
+
+        # read data and compare
+        info_written = read_info('resources/test_resources/temp/temp_is_in_filter_polygon_not_in_map.nc')
+        info_expected = read_info('resources/test_resources/netcdf/test_file_50_50.nc')
+
+        self.assertTrue((info_written[0] == info_expected[0]).all())
+        self.assertTrue((info_written[1] == info_expected[1]).all())
+        self.assertTrue((info_written[2] == info_expected[2]).all())
+
+        os.remove('resources/test_resources/temp/temp_is_in_filter_polygon_not_in_map.nc')
+
+    def test_polygon_not_in_map_is_not_in_filter(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+
+        self.engine.refresh_with_model_2d('resources/test_resources/cpt/colors_0_100_200.cpt',
+                                          'resources/test_resources/netcdf/test_file_50_50.nc')
+
+        # load list of polygons
+        self.engine.load_polygon_from_shapefile('resources/test_resources/polygons/'
+                                                'shape_three_polygons_south_america.shp')
+
+        # apply transformation with filters
+        self.engine.transform_points(polygon_id='Polygon 0',
+                                     model_id=self.engine.get_active_model_id(),
+                                     min_height=100,
+                                     max_height=200,
+                                     transformation_type='linear',
+                                     filters=[('is_not_in', 'Polygon 1'),
+                                              ('is_in', 'Polygon 2')])
+
+        # export model to compare data
+        self.engine.export_model_as_netcdf(self.engine.get_active_model_id(),
+                                           'resources/test_resources/temp/temp_is_in_filter_polygon_not_in_map')
+
+        # read data and compare
+        info_written = read_info('resources/test_resources/temp/temp_is_in_filter_polygon_not_in_map.nc')
+        info_expected = read_info('resources/test_resources/netcdf/test_file_50_50.nc')
+
+        self.assertTrue((info_written[0] == info_expected[0]).all())
+        self.assertTrue((info_written[1] == info_expected[1]).all())
+        self.assertTrue((info_written[2] == info_expected[2]).all())
+
+        os.remove('resources/test_resources/temp/temp_is_in_filter_polygon_not_in_map.nc')
+
 
 if __name__ == '__main__':
     unittest.main()
