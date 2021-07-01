6e891c790c834f45ede7a8433195154c9c43806c
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-07-01 12:34:44 -0400

    Added tests for the export of files in netcdf format.

--- test/output/test_export_netcdf.py
+++ test/output/test_export_netcdf.py
@@ -21,9 +21,11 @@ File with test related to the functionality of exporting netcdf files.
 import unittest
 import numpy as np
 import os
+import shutil
 
 from src.output.netcdf_exporter import NetcdfExporter
 from src.input.NetCDF import read_info
+from src.error.export_error import ExportError
 
 
 class TestExportNetcdfFile(unittest.TestCase):
@@ -45,6 +47,66 @@ class TestExportNetcdfFile(unittest.TestCase):
         self.assertTrue((generated_z == z).all(), f'Data loaded is not equal to data expected. {generated_z} != {z}')
 
         os.remove('resources/test_resources/temp/test_model_temp.nc')
+
+    def test_export_file_no_keys(self):
+        exporter = NetcdfExporter()
+
+        # Should not modify the file in any way
+        with self.assertRaises(ExportError):
+            exporter.modify_heights_existent_netcdf_file(np.array([]),
+                                                         'resources/test_resources/netcdf/file_no_keys.nc')
+
+    def test_export_one_dimension_heights(self):
+        exporter = NetcdfExporter()
+
+        # Copy file to modify to not modify the original file
+        shutil.copy('resources/test_resources/netcdf/test_file_4.nc',
+                    'resources/test_resources/temp/one_dimension_file.nc')
+
+        # Read the info of the file and generate new data with the same dimension that the one stored in the file
+        x, y, z = read_info('resources/test_resources/temp/one_dimension_file.nc')
+        new_data = np.zeros(z.shape)
+
+        # Modify the data of the file and read it to compare
+        exporter.modify_heights_existent_netcdf_file(new_data,
+                                                     'resources/test_resources/temp/one_dimension_file.nc')
+        new_x, new_y, new_z = read_info('resources/test_resources/temp/one_dimension_file.nc')
+
+        # Compare the data of the file
+        self.assertTrue((new_x == x).all())
+        self.assertTrue((new_y == y).all())
+        self.assertTrue((new_z == new_data).all())
+
+        # Delete temporal files generated in the test
+        os.remove('resources/test_resources/temp/one_dimension_file.nc')
+
+    def test_export_file_actual_range_metadata(self):
+        """
+        Test the export of a file with the metadata actual_range inside the variable that stores the heights of the
+        maps.
+        """
+        exporter = NetcdfExporter()
+
+        # Copy file to modify to not modify the original file
+        shutil.copy('resources/test_resources/netcdf/test_file_1.nc',
+                    'resources/test_resources/temp/actual_range_metadata_file.nc')
+
+        # Read the info of the file and generate new data with the same dimension that the one stored in the file
+        x, y, z = read_info('resources/test_resources/temp/actual_range_metadata_file.nc')
+        new_data = np.zeros(z.shape)
+
+        # Modify the data of the file and read it to compare
+        exporter.modify_heights_existent_netcdf_file(new_data,
+                                                     'resources/test_resources/temp/actual_range_metadata_file.nc')
+        new_x, new_y, new_z = read_info('resources/test_resources/temp/actual_range_metadata_file.nc')
+
+        # Compare the data of the file
+        self.assertTrue((new_x == x).all())
+        self.assertTrue((new_y == y).all())
+        self.assertTrue((new_z == new_data).all())
+
+        # Delete temporal files generated in the test
+        os.remove('resources/test_resources/temp/actual_range_metadata_file.nc')
 
 
 if __name__ == '__main__':
