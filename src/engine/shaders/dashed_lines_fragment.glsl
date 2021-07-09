6e0fd9622c1dd0b2e9aef2955413b698f7a3f0a9
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-07-09 12:01:01 -0400

    Docs

--- src/engine/shaders/dashed_lines_fragment.glsl
+++ src/engine/shaders/dashed_lines_fragment.glsl
@@ -23,17 +23,21 @@ out vec4 outColor;
 flat in vec3 startPos;
 in vec3 vertPos;
 
+// Color of the line
 uniform vec4 lines_color;
 
+// Uniforms that store data for the dashed logic
 uniform vec2  u_resolution;
 uniform float u_dashSize;
 uniform float u_gapSize;
 
 void main()
 {
+    // Calcualte the direction and the distance of the line to be drawed
     vec2  dir  = (vertPos.xy-startPos.xy) * u_resolution/2.0;
     float dist = length(dir);
 
+    // Discard fragments to create the effect of the dashed line
     if (fract(dist / (u_dashSize + u_gapSize)) > u_dashSize/(u_dashSize + u_gapSize)){
         discard;
     }
