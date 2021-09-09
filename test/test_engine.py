8bded003cbb1acf31e823248027445d5f10cd4a4
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-09-09 12:53:25 -0300

    Added tests for the model information method on the Engine class.

--- test/test_engine.py
+++ test/test_engine.py
@@ -20,9 +20,11 @@ Module with test related to the engine of the program.
 """
 
 import unittest
+import warnings
 
 from src.engine.engine import Engine
 from src.program.program import Program
+from src.utils import dict_to_serializable_dict, json_to_dict
 
 
 class TestViewMode(unittest.TestCase):
@@ -53,6 +55,55 @@ class TestViewMode(unittest.TestCase):
         self.assertEqual('2D',
                          program.get_view_mode(),
                          'Mode was not changed to 2D after calling set_program_view_mode')
+
+
+class TestModelInformation(unittest.TestCase):
+    engine: Engine = None
+    program: Program = None
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        """
+        Method executed once before the tests begins.
+
+        Create program and load a map into it.
+        """
+        warnings.simplefilter('ignore', category=ResourceWarning)
+        cls.engine = Engine()
+        cls.engine.should_use_threads(False)
+        cls.program = Program(cls.engine)
+
+        cls.engine.load_netcdf_file('resources/test_resources/cpt/colors_0_100_200.cpt',
+                                    'resources/test_resources/netcdf/test_file_1.nc')
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """
+        Method executed once after all tests.
+        """
+        cls.program.close()
+
+    def test_information_keys(self):
+        model_list = self.engine.get_model_list()
+        self.assertEqual(1,
+                         len(model_list),
+                         'Model list does not have only one element.')
+        self.assertEqual(
+            ['height_array', 'coordinates_array', 'projection_matrix', 'showed_limits', 'shape', 'filename'],
+            list(self.engine.get_model_information(model_list[0]).keys()),
+            'Keys in the dictionary are not equal to the expected keys.'
+        )
+
+    def test_information_values(self):
+        model_list = self.engine.get_model_list()
+        model_information = self.engine.get_model_information(model_list[0])
+
+        model_information_serialized = dict_to_serializable_dict(model_information)
+        model_information_expected = json_to_dict('resources/test_resources/expected_data/json_data/model_info.json')
+
+        self.assertEqual(model_information_serialized,
+                         model_information_expected,
+                         'Model information generated is not equal to the expected.')
 
 
 if __name__ == '__main__':
