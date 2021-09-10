81c05e91b9cb79d888773865a3743b007ab2990e
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-09-10 12:45:55 -0300

    Updated comment.

--- src/engine/scene/scene.py
+++ src/engine/scene/scene.py
@@ -727,10 +727,8 @@ class Scene:
 
             # Draw all the polygons
             for polygon, draw_order in zip(self.__polygon_draw_order, range(len(self.__polygon_draw_order))):
-                # Change the height of the polygon depending on the draw order
+                # Change the height of the polygon depending on the draw order and draw them
                 self.__polygon_hash[polygon].set_z_offset(len(self.__polygon_draw_order) - draw_order + 1)
-
-                # Draw the polygons on the scene
                 self.__polygon_hash[polygon].draw()
 
         elif self.__engine.get_program_view_mode() == '3D':
