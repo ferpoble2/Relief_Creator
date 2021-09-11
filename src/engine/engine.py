a2f202df6dbcf4899f1fc920571e8a7635ea52be
Sergio Alvarez Medina <ser.alvarez1998@gmail.com>
2021-09-10 22:26:29 -0300

    Updated method reload_models.
    Make method set the program loading state False at the end of the 'then' routine.

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -1292,7 +1292,7 @@ class Engine:
         # noinspection PyMissingOrEmptyDocstring
         def then_routine():
             self.optimize_gpu_memory()  # async function
-            # self.program.set_loading(False)
+            self.program.set_loading(False)
 
         self.scene.reload_models_async(then_routine)
 
